c64bd9127f137479c5cc9320fc5876b1ccd624e8
Igor dos Reis <igorreis@gmail.com>
2023-01-28 15:04:11 -0300

    Fix: mudando a importacao do bcryptjs.

--- app/backend/src/utils/bcrypt.util.ts
+++ app/backend/src/utils/bcrypt.util.ts
@@ -1,8 +1,8 @@
-import { compareSync } from 'bcryptjs';
+import * as bcryptjs from 'bcryptjs';
 import HttpException from './httpException.util';
 
 const validatePasswordUtil = (reqPassword: string, recoveredPassword: string): void => {
-  const isInvalid = !compareSync(reqPassword, recoveredPassword);
+  const isInvalid = !bcryptjs.compareSync(reqPassword, recoveredPassword);
 
   if (isInvalid) throw new HttpException(401, 'Incorrect email or password');
 };
