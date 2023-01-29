bc36f793add287dd0c9e06957507e604b41a74c2
Igor dos Reis <igorreis@gmail.com>
2023-01-28 21:23:17 -0300

    Fix: Adicionado 'start' no CMD do dockerfile de backend.

--- app/backend/src/utils/httpException.util.ts
+++ app/backend/src/utils/httpException.util.ts
@@ -1,9 +1,11 @@
 class HttpException extends Error {
   status: number;
+  timestamp: string;
 
   constructor(status: number, message: string) {
     super(message);
     this.status = status;
+    this.timestamp = new Date().toLocaleString('pt-BR', { timeZone: 'America/Sao_Paulo' });
   }
 }
 
